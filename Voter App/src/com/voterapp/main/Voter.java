1d6417c87228b4c6592e17cebff4978549264db3
Gayathri S <gayathri.s@acheron-tech.com>
2021-08-20 10:33:34 +0530

    First Commit

--- Voter App/src/com/voterapp/main/Voter.java
+++ Voter App/src/com/voterapp/main/Voter.java
@@ -12,24 +12,22 @@ public class Voter {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter your age");
 		int age=s.nextInt();
-		/*
-		 * try { booth.checkAge(age); } catch (UnderAgeException e1) {
-		 * e1.printStackTrace(); }
-		 */
-		/*
-		 * try { booth.checkAge(age); } catch (UnderAgeException e2) {
-		 * e2.printStackTrace(); }
-		 */
 		System.out.println("Enter your localitiy");
-	    String local=s.next();
+		s.nextLine();
+	    String localname=s.nextLine();
 	    System.out.println("Enter your vid");
 		int vid=s.nextInt();
+		System.out.println(localname);
             try {
-		booth.checkEligibility(age,local,vid);
+		       booth.checkEligibility(age,localname,vid);
             }
             catch(NotEligibleException e) {
+            	
             	System.out.println(e.getMessage());
             }
+            finally {
+    			System.out.println("Have a nice Day...");
+    		}
 		s.close();
 
 	}
--- Voter App/src/com/voterapp/service/ElectionBooth.java
+++ Voter App/src/com/voterapp/service/ElectionBooth.java
@@ -3,60 +3,62 @@ package com.voterapp.service;
 import java.util.Scanner;
 
 import com.voterapp.exception.*;
- public class ElectionBooth {
 
-	 String[] localities= {"ABC Nagar","KK Nagar","Anna Nagar"};
-	public boolean checkAge(int age)throws UnderAgeException{
-		if(age<18) {
+public class ElectionBooth {
+
+	String[] localities = { "ABC Nagar", "KK Nagar", "Anna Nagar" };
+
+	public boolean checkAge(int age) throws UnderAgeException {
+		if (age <= 18) {
 			throw new UnderAgeException("Age is less then 18...");
 		}
 		return true;
 	}
-	public boolean checkLocality(String locality)throws LocalityFileNotException{
-		for(String loc:localities) {
-			if(!loc.equals(locality)) {
-				throw new LocalityFileNotException("Locality is not available");
-				
+
+	public boolean checkLocality(String locality) throws LocalityFileNotException {
+		boolean isPresent = false;
+		for (String loc : localities) {
+			if (loc.equals(locality)) {
+				isPresent = true;
 			}
 		}
-		return true;
+		if (isPresent == true) {
+			return true;
+		} else {
+			throw new LocalityFileNotException("Locality is not available");
+
+		}
 	}
-	public boolean checkVoterID(int vid)throws NoVoterIDException{
-		if(vid<1000 || vid>9000) {
+
+	public boolean checkVoterID(int vid) throws NoVoterIDException {
+		if (vid < 1000 || vid > 9000) {
 			throw new NoVoterIDException("Voter id is not available");
 		}
 		return true;
 	}
-	public boolean checkEligibility(int age,String locality,int vid ) throws NotEligibleException{
-		boolean result;
-		//ElectionBooth booth=new ElectionBooth();
-		try(Scanner s=new Scanner(System.in);){
-			result = checkAge(age);
-			System.out.println("Enter the locality");
-			try {
-				//System.out.println("Enter the locality");
-				result=checkLocality(locality);
-			}catch (LocalityFileNotException e) {
-				System.out.println(e.getMessage());
-				throw e;
-			}
-			try {
-				result=checkVoterID(vid);
-			}
-			catch (NoVoterIDException e1) {
-				System.out.println(e1.getMessage());
-				throw e1;
-			}
-		  }catch(UnderAgeException e3) {
-			  System.out.println(e3.getMessage());
-              throw e3;
-		  }
-		finally {
-			System.out.println("Welcome voter");
+
+	public boolean checkEligibility(int age, String locality, int vid) throws NotEligibleException {
+
+		try {
+			checkAge(age);
+		} catch (UnderAgeException e) {
+
+			throw e;
 		}
-	return true;
-  }
-		
- }
+		try {
+			checkLocality(locality);
+		} catch (LocalityFileNotException e1) {
 
+			throw e1;
+		}
+		try {
+			checkVoterID(vid);
+		} catch (NoVoterIDException e2) {
 
+			throw e2;
+		}
+
+		return true;
+	}
+
+}
